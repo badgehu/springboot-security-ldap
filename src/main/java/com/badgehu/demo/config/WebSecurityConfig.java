81dea7b9b9eac0f2c419bdf95a7015f3d9cf6f92
HuXiaoHui <1334660882@qq.com>
2019-02-22 10:59:52 +0800

    自定义ldap授权，验证后使用本地数据库信息进行授权

--- src/main/java/com/badgehu/demo/config/WebSecurityConfig.java
+++ src/main/java/com/badgehu/demo/config/WebSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.badgehu.demo.config;
 
+import com.badgehu.demo.dao.PermissionDao;
+import com.badgehu.demo.dao.UserDao;
+import com.badgehu.demo.domain.Permission;
+import com.badgehu.demo.domain.SysUser;
 import com.badgehu.demo.security.MyCustomUserService;
 import com.badgehu.demo.security.MyFilterSecurityInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +11,23 @@ import org.springframework.boot.autoconfigure.ldap.LdapProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +39,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyFilterSecurityInterceptor myFilterSecurityInterceptor;
+
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    PermissionDao permissionDao;
 
     @Bean
     UserDetailsService customUserService(){
@@ -59,19 +79,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         String url = String.format("%s/%s", ldapProperties.getUrls()[0], ldapProperties.getBase());
 
         auth
-                .userDetailsService(customUserService())
-                .passwordEncoder(new MyPasswordEncoder())//user Details Service验证
-                .and()
-
-
                 .ldapAuthentication()
                 .contextSource()
                 .url(url)
                 .managerDn(ldapProperties.getUsername())
                 .managerPassword(ldapProperties.getPassword())
                 .and()
-                .userDnPatterns("uid={0}","cn={0}","cn={0},ou=tech");
+                .userDnPatterns("uid={0}","cn={0}","cn={0},ou=tech")
+                .ldapAuthoritiesPopulator(new LdapAuthoritiesPopulator() {
+                    @Override
+                    public Collection<? extends GrantedAuthority> getGrantedAuthorities(DirContextOperations userData, String username) {
+                        SysUser user = userDao.findByUserName(username);
+                        if (user != null) {
+                            List<Permission> permissions = permissionDao.findByAdminUserId(user.getId());
+                            List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+                            for (Permission permission : permissions) {
+                                if (permission != null && permission.getName()!=null) {
 
+                                    GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(permission.getName());
+                                    //1：此处将权限信息添加到 GrantedAuthority 对象中，在后面进行全权限验证时会使用GrantedAuthority 对象。
+                                    grantedAuthorities.add(grantedAuthority);
+                                    System.out.println(permission.getName());
+                                }
+                            }
+                            return grantedAuthorities;
+                        } else {
+                            throw new UsernameNotFoundException("admin: " + username + " do not exist!");
+                        }
+                    }
+                });
     }
 
     /*@Autowired
